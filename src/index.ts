77d7fad4dc2af1e189b7e83c1b732bc3962fd7e8
talinsk <63950363+talinsk@users.noreply.github.com>
2021-07-16 13:37:49 +0700

    lesson1

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { renderSearchFormBlock } from './search-form.js'
+import { renderSearchStubBlock } from './search-results.js'
+import { renderUserBlock } from './user.js'
+import { renderToast } from './lib.js'
+
+window.addEventListener('DOMContentLoaded', () => {
+  renderUserBlock('Ivana Ivanova', '/img/avatar.png', 5)
+  renderSearchFormBlock(new Date(), new Date())
+  renderSearchStubBlock()
+  renderToast(
+    {text: 'Это пример уведомления. Используйте его при необходимости', type: 'success'},
+    {name: 'Понял', handler: () => {console.log('Уведомление закрыто')}}
+  )
+})
